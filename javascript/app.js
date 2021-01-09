ab6f1f712d42d648b0f597cc6bbd443f87b5bd04
benhovinga <agnivohneb@gmail.com>
2021-01-09 00:46:08 -0400

    Rebuild FontSize into app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,100 +1,30 @@
-function year() {
-  date = new Date();
-  return date.getFullYear();
-}
-
-function newyear() {
-  date = new Date();
-  return date.getFullYear() + 1;
-}
-
-function getTime() {
-  now = new Date();
-
-  //ENTER BELOW THE DATE YOU WISH TO COUNTDOWN TO
-  later = new Date("Jan 1 " + newyear() + " 0:00:00");
-
-  days = (later - now) / 1000 / 60 / 60 / 24;
-  daysRound = Math.floor(days);
-  hours = (later - now) / 1000 / 60 / 60 - 24 * daysRound;
-  hoursRound = Math.floor(hours);
-  minutes = (later - now) / 1000 / 60 - 24 * 60 * daysRound - 60 * hoursRound;
-  minutesRound = Math.floor(minutes);
-  seconds =
-    (later - now) / 1000 -
-    24 * 60 * 60 * daysRound -
-    60 * 60 * hoursRound -
-    60 * minutesRound;
-  secondsRound = Math.round(seconds);
-
-  if (secondsRound <= 9) {
-    document.getElementById("s").innerHTML = "0" + secondsRound;
-  } else {
-    document.getElementById("s").innerHTML = secondsRound;
-  }
-  if (minutesRound <= 9) {
-    document.getElementById("m").innerHTML = "0" + minutesRound;
-  } else {
-    document.getElementById("m").innerHTML = minutesRound;
-  }
-  if (hoursRound <= 9) {
-    document.getElementById("h").innerHTML = "0" + hoursRound;
-  } else {
-    document.getElementById("h").innerHTML = hoursRound;
-  }
-  if (daysRound <= 999) {
-    document.getElementById("d").innerHTML = daysRound;
-  }
-
-  if (daysRound <= 99) {
-    document.getElementById("d").innerHTML = "0" + daysRound;
-  }
-  if (daysRound <= 9) {
-    document.getElementById("d").innerHTML = "00" + daysRound;
-  }
-
-  newtime = window.setTimeout("getTime();", 1000);
-}
-
-var min = 1;
-var max = 3000;
-function increaseFontSize() {
-  var p = document.getElementsByTagName("p");
-  for (i = 0; i < p.length; i++) {
-    if (p[i].style.fontSize) {
-      var s = parseInt(p[i].style.fontSize.replace("px", ""));
-    } else {
-      var s = 12;
-    }
-    if (s != max) {
-      s += 5;
-    }
-    p[i].style.fontSize = s + "px";
+class FontSize {
+  constructor(clock, defaultSize) {
+    let min = 5; // Can't go to 0 or negative
+    let max = 500; // Really large font!
+    let steps = 5;
+    let current =
+      defaultSize ||
+      parseInt(window.getComputedStyle(clock).getPropertyValue("font-size"));
+    this.set = function (newSize) {
+      // Set new font size
+      if (newSize >= min || newSize <= max) current = newSize;
+      else console.log("Font size is not within range: " + min + "-" + max);
+      clock.style.fontSize = current + "px";
+    };
+    this.increment = function () {
+      // Increase the size
+      if (current < max) current += steps;
+      else console.log("Maximum font size of '" + max + "' has been reached.");
+      clock.style.fontSize = current + "px";
+    };
+    this.decrement = function () {
+      // Decrease the size
+      if (current > min) current -= steps;
+      else console.log("Minimum font size of '" + min + "' has been reached.");
+      clock.style.fontSize = current + "px";
+    };
   }
 }
-function decreaseFontSize() {
-  var p = document.getElementsByTagName("p");
-  for (i = 0; i < p.length; i++) {
-    if (p[i].style.fontSize) {
-      var s = parseInt(p[i].style.fontSize.replace("px", ""));
-    } else {
-      var s = 12;
-    }
-    if (s != min) {
-      s -= 5;
-    }
-    p[i].style.fontSize = s + "px";
-  }
-}
-function FontSize(thesize) {
-  var p = document.getElementsByTagName("p");
-  for (i = 0; i < p.length; i++) {
-    if (p[i].style.fontSize) {
-      var s = parseInt(p[i].style.fontSize.replace("px", ""));
-    } else {
-      var s = 12;
-    }
-    s = thesize;
-    p[i].style.fontSize = s + "px";
-  }
-}
+
+const fs = new FontSize(document.getElementById("clock"));
